262bf68bad94ea1fe6d25dfe1c7f764a6ccf4175
mauricio suarez <maurohbn@hotmail.es>
2017-03-15 17:46:55 -0500

    update

--- resources/views/layouts/app/footer.blade.php
+++ resources/views/layouts/app/footer.blade.php
@@ -1,107 +1,110 @@
-<div class=" remove-padding text-white">
-    <div class="col-sm-12 padding-20 bg-primary">
-        <div class="block-content-full">
+<div class="bg-primary">
+    <div class="content-boxed overflow-hidden">
+        <div class=" row content text-white">
             <div class="col-sm-6   ">
                 <h4 class="h3"> Signup for Newsletter</h4>
-                <p>Affronting everything discretion men now own did. Still round match we to. Frankbness pronounce
-                   daughters remainder extensive has but.</p>
+                <p>Affronting everything discretion men now own did. Still round match we to. Frankbness
+                   pronounce daughters remainder extensive has but.</p>
             </div>
             <div class="col-sm-6 overflow-hidden">
                 <form class="form-horizontal push-20-t">
                     <div class="form-group  ">
-                        <div class="col-sm-12">
-                            <div class="input-group">
-                                <input class="form-control" type="email" id="example-input2-group2"
-                                       name="example-input2-group2" placeholder="Email">
-                                <span class="input-group-btn">
-<button class="btn btn-minw btn-primary-darkest" type="button">Submit</button>
-</span>
-                            </div>
+                        <div class="input-group">
+                            <input class="form-control" type="email" name="email" placeholder="Email">
+                            <span class="input-group-btn">
+                                <button class="btn btn-minw btn-primary-darkest" type="button">Submit</button>
+                            </span>
                         </div>
                     </div>
-                
                 </form>
             </div>
         </div>
     </div>
 </div>
-
-<div class="col-sm-12 text-gray">
-    <div class="col-lg-4 col-sm-4 push-10-t text-center">
-        <img class="img-responsive" src="{{asset('img/logo-naranja.svg')}}">
-        <p class="push-15-t text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, architecto earum? Aspernatur cumque
-            eaque,
-            enim est explicabo fugiat maxime necessitatibus nemo nisi non obcaecati, odio quam quas quos ullam ut.
-        </p>
-        <div class="item-social">
-            <a class="item item-social push-10-r item-circle bg-gray-lighter">
-                <i class="fa fa-facebook text-gray-dark"></i>
-            </a>
-            
-            <a class="item item-circle push-10-r bg-gray-lighter">
-                <i class="fa fa-twitter text-gray-dark"></i>
-            </a>
-            <a class="item item-circle push-10-r bg-gray-lighter">
-                <i class="fa fa-google-plus text-gray-dark"></i>
-            </a>
-            <a class="item item-circle push-10-r bg-gray-lighter">
-                <i class="fa fa-youtube text-gray-dark"></i>
-            </a>
-            <a class="item item-circle  bg-gray-lighter">
-                <i class="fa fa-envelope-o text-gray-dark"></i>
-            </a>
+<div class="content-boxed">
+    <div class="col-sm-12 text-gray">
+        <div class="col-lg-4 col-sm-4 push-10-t text-center">
+            <img class="logo img-responsive" src="{{asset('img/logo-naranja.svg')}}">
+            <p class="push-15-t text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, architecto earum? Aspernatur cumque
+                eaque,
+                enim est explicabo fugiat maxime necessitatibus nemo nisi non obcaecati, odio quam quas quos ullam ut.
+            </p>
+            <div class="item-social">
+                <a class="item item-social push-10-r item-circle bg-gray-lighter">
+                    <i class="fa fa-facebook text-gray-dark"></i>
+                </a>
+                
+                <a class="item item-circle push-10-r bg-gray-lighter">
+                    <i class="fa fa-twitter text-gray-dark"></i>
+                </a>
+                <a class="item item-circle push-10-r bg-gray-lighter">
+                    <i class="fa fa-google-plus text-gray-dark"></i>
+                </a>
+                <a class="item item-circle push-10-r bg-gray-lighter">
+                    <i class="fa fa-youtube text-gray-dark"></i>
+                </a>
+                <a class="item item-circle  bg-gray-lighter">
+                    <i class="fa fa-envelope-o text-gray-dark"></i>
+                </a>
+            </div>
+        
+        
+        
         </div>
-    
-    
-    
-    </div>
-    <div class="col-lg-2 col-sm-2 text-left  push-50-t">
-        <p class="h3"> About</p>
-        <br>
-        <a>About us</a><br>
-        <a>Resposability Child</a><br>
-        <a>FAQ</a><br>
-        <a>Login</a><br>
-        <a>Register</a><br>
-        <a>Terms and conditions</a>
-    </div>
-    <div class="col-lg-2 col-sm-2 text-left  push-50-t">
-        <p class="h3"> About</p>
-        <br>
-        <a>About us</a><br>
-        <a>Resposability Child</a><br>
-        <a>FAQ</a><br>
-        <a>Login</a><br>
-        <a>Register</a><br>
-        <a>Terms and conditions</a>
-    </div>
-    <div class="col-lg-4 col-sm-4 text-center push-50-t">
-        <h4 class="h3 font-w600">Have Questions?</h4>
-        <span class="item push-20-t item-circle bg-gray-lighter">
+        <div class="col-lg-2 col-sm-2 text-left  push-20-t">
+            <p class="h3 text-white "> About</p>
+            <br>
+            <ul class="list-unstyled">
+                <li> <a>About us</a></li>
+                <li> <a>Resposability Child</a></li>
+                <li> <a>FAQ</a></li>
+                <li> <a>Login</a></li>
+                <li><a>Register</a></li>
+                <li> <a>Terms and conditions</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-2 col-sm-2 text-left  push-20-t">
+            <p class="h3 text-white "> About</p>
+            <br>
+            <ul class="list-unstyled">
+                <li> <a>About us</a></li>
+                <li> <a>Resposability Child</a></li>
+                <li> <a>FAQ</a></li>
+                <li> <a>Login</a></li>
+                <li><a>Register</a></li>
+                <li> <a>Terms and conditions</a></li>
+            </ul>
+            
+        </div>
+        <div class="col-lg-4 col-sm-4 text-center push-20-t">
+            <h4 class="h3 text-white font-w600">Have Questions?</h4>
+            <span class="item push-20-t item-circle bg-gray-lighter">
             <i class="fa fa-phone text-gray-dark"></i>
         </span>
-        <p class="push-20-t h3">+1-202-555-0173</p>
-        <p class="">
-            info@travelongocolombia.com
-            <br>
-            Monday to Friday <br> 9.00am - 7.30pm
-        </p>
+            <p class="push-20-t h3">+1-202-555-0173</p>
+            <p class="">
+                info@travelongocolombia.com
+                <br>
+                Monday to Friday <br> 9.00am - 7.30pm
+            </p>
+        </div>
     </div>
-</div>
-
-<div class="">
-    <div class="col-sm-12 border-white-op-t push-15-t content-mini content-mini-full ">
+    
+    <div class="">
+        <div class="col-sm-12 border-white-op-t push-15-t content-mini content-mini-full ">
        
             <span class="text-white">
                 Copyright © 2017 Travelongo Colombia All Rights Reserved.
             </span>
-        
-        <span class="text-white pull-right">
+            
+            <span class="text-white pull-right">
                 Made with <i class="fa fa-heart text-city"></i> in Colombia by <a
-                    href="http://ziuxlab.com">Ziuxlab.com</a>
+                        href="http://ziuxlab.com">Ziuxlab.com</a>
             </span>
-    
-    
+        
+        
+        </div>
     </div>
 </div>
+
