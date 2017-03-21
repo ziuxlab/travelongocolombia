d273d7ae4b559888190a6d209988d1c192d49abd
mauricio suarez <maurohbn@hotmail.es>
2017-03-21 18:38:37 -0500

    update

--- resources/views/layouts/admin/partials/_ckeditor.blade.php
+++ resources/views/layouts/admin/partials/_ckeditor.blade.php
@@ -6,11 +6,7 @@
         filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token={{csrf_token()}}',
         filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
         filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token={{csrf_token()}}',
-        extraAllowedContent: '*[*]{*}(*)'
+        extraAllowedContent: '*[*]{*}(*)',
     });
-
-    CKEDITOR.editorConfig = function( config ) {
-        config.extraAllowedContent = true;
-        config.allowedContent = true;// remove '[id]', if you don't want IDs for HTML tags
-    }
+    CKEDITOR.config.allowedContent = true;
 </script>
