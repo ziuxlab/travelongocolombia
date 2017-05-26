ecc23f90c01ba0ddb19aa02127a382c6e4569934
mauricio suarez <mauroziux@gmail.com>
2017-05-26 11:43:47 -0500

    update

--- resources/views/admin/packages/packages_index.blade.php
+++ resources/views/admin/packages/packages_index.blade.php
@@ -50,7 +50,7 @@
                                    data-original-title="Editar Paquete"><i class="fa fa-pencil"></i></a>
                                <button class="js-swal-confirm btn btn-xs btn-default" type="button"
                                         data-toggle="tooltip" data-id="{{ $package->id }}" title=""
-                                        data-original-title="Eliminar usuario"><i class="fa fa-times"></i>
+                                        data-original-title="Eliminar Paquete"><i class="fa fa-times"></i>
                                    {!! Form::open(['action'=> ['ProductController@destroy',$package->id],'method'=>'delete','id'=>'item_'.$package->id]) !!}
                                    {!! Form::close() !!}
                                </button>
