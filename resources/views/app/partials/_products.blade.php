0484606ab4f7d5a8268988e8780bd3648825d255
mauricio suarez vega <msuarez@gokpaital.com>
2017-11-03 19:48:29 -0500

    precio en listas de prodcutos

--- resources/views/app/partials/_products.blade.php
+++ resources/views/app/partials/_products.blade.php
@@ -1,33 +1,33 @@
 <div class="block block-bordered  flex overflow-hidden">
-    
+
     <div class="col-md-4 col-sm-3 col-xs-12  bg-image remove-padding"
          style="background-image: url('{{asset(count($product->photos)>0 ? $product->photos->sortBy('order')->first()->img : 'img/banner/about-us.jpg')}}'); background-position-x: 50%;">
         @if($product->type == 0)
-            <a href="{{url(trans('general.packages').'/'.$product->slug_url)}}">
-                @elseif($product->type == 1)
-                    <a href="{{url(trans('general.activities').'/'.$product->slug_url)}}">
-                        @elseif($product->type == 2)
-                            <a href="{{url(trans('general.hotels').'/'.$product->slug_url)}}">
-                                @endif
-                                <div class="mheight-200">
-                                    @if($product->discount > 0 or $product->discount <> null)
-                                        <div class=" ribbon ribbon-bookmark ribbon-primary ribbon-left">
-                                            <div class="ribbon-box font-w600">
-                                                {{$product->discount}}% Off
-                                            </div>
-                                        </div>
-                                    @endif
-                                </div>
-                            </a>
+        <a href="{{url(trans('general.packages').'/'.$product->slug_url)}}">
+        @elseif($product->type == 1)
+        <a href="{{url(trans('general.activities').'/'.$product->slug_url)}}">
+        @elseif($product->type == 2)
+        <a href="{{url(trans('general.hotels').'/'.$product->slug_url)}}">
+        @endif
+            <div class="mheight-200">
+                @if($product->discount > 0 or $product->discount <> null)
+                    <div class=" ribbon ribbon-bookmark ribbon-primary ribbon-left">
+                        <div class="ribbon-box font-w600">
+                            {{$product->discount}}% Off
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </a>
     </div>
-    
+
     <div class="col-md-6 col-sm-6 col-xs-12 border-black-op-r border-black-op-b  content">
         @if($product->type == 0)
             <a href="{{url(trans('general.packages').'/'.$product->slug_url)}}">
                 <h2 class="h3 text-capitalize">{{$product->tittle}}</h2>
             </a>
             <p class="push-10">{{mb_substr($product->short_description,0,171,'UTF-8')}}...</p>
-        
+
         @endif
         @if($product->type == 1)
             <a href="{{url(trans('general.activities').'/'.$product->slug_url)}}">
@@ -56,12 +56,20 @@
     </div>
     <div class="col-md-2 col-sm-3 col-xs-12  content content-full text-center flex-center">
         <div>
-        <!--
+
             <div class="h1 font-w700 ">
-                ${{number_format($product->price_adults * ( 1 - ($product->discount/100)))}}*
+                @if($product->type == 2)
+                    @if($product->kindsHotel()->where('quantity','>',0)->first())
+                        ${{number_format($product->kindsHotel()->where('quantity','>',0)->first()->pivot->price * ( 1 - ($product->discount/100)))}}*
+                    @else
+                        <small style="font-size: 18px;">No vacancy</small>
+                    @endif
+                    @else
+                        ${{number_format($product->price_adults * ( 1 - ($product->discount/100)))}}*
+                    @endif
             </div>
             <div>*@lang('general.person'){{$product->type == 2 ? '|*'.trans('general.night'):''}}</div>
-            -->
+
             @if($product->type == 0)
                 <a href="{{url(trans('general.packages').'/'.$product->slug_url)}}"
                    class="btn btn-primary push-20-t text-capitalize">@lang('general.view details')</a>
